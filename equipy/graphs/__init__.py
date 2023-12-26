ed47cd7766b1d2b7c7a95ce4a4c9515fd1910f6f
Suzie14 <suzie.grondin@gmail.com>
2023-12-26 16:18:05 +0100

    customized

--- equipy/graphs/__init__.py
+++ equipy/graphs/__init__.py
@@ -1,4 +1,4 @@
-from ._arrow_plot import fair_arrow_plot, _fair_custimized_arrow_plot, fair_multiple_arrow_plot
+from ._arrow_plot import fair_arrow_plot, fair_multiple_arrow_plot
 from ._density_plot import fair_density_plot
 from ._waterfall_plot import fair_waterfall_plot
 
