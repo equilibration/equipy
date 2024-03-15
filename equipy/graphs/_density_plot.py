correct errors for calculation of performance metrics when dealing with binary classification

equipy/graphs/_density_plot.py
@@ -54,8 +54,7 @@ def fair_density_plot(sensitive_features_calib: np.ndarray,
         }
     >>> sensitive_features = [[sensitive_features_of_ind_1_values], [sensitive_feature_of_ind_2_values], ...]
     """
-
-    epsilon = None
+    
     exact_wst = MultiWasserstein()
     exact_wst.fit(y_calib, sensitive_features_calib)
     y_final_fair = exact_wst.transform(y_test, sensitive_features_test, epsilon=epsilon)

equipy/metrics/_performance_metrics.py
@@ -7,7 +7,7 @@ Computation of the performance (i.e. measurement of the similarity between predi
 import numpy as np
 from typing import Callable, Optional, Union
 from sklearn.metrics import mean_squared_error
-from ..utils.checkers import _check_metric
+from ..utils.checkers import _check_metric, _check_type, _check_positive_class
 
 
 def performance(y_true: np.ndarray, y_pred: np.ndarray, metric: Callable = mean_squared_error) -> float:
@@ -78,10 +78,11 @@ def performance_dict(y_true: np.ndarray, y_fair_dict: dict[str, np.ndarray], met
     >>> print(performance_values)
     {'Base model': 8.666666666666666, 'sensitive_feature_1': 125.66666666666667, 'sensitive_feature_2': 282.0}
     """
-
+    _check_type(y_true, y_fair_dict, threshold=threshold)
     if threshold is not None:
+        _check_positive_class(y_true, positive_class)
         negative_class = list(set(y_true) - {positive_class})[0]
-
+       
     performance_dict = {}
     for key in y_fair_dict.keys():
         if threshold is not None:

equipy/utils/checkers.py
@@ -49,7 +49,7 @@ def _check_type(y_true: np.ndarray, y_dict: Dict[Any, Dict[Any, Any]], threshold
         If y_true and the values in y_dict are not of the same type.
     """
     type_y_true = get_subtype(y_true)
-    type_y_fair = get_subtype(list(list(y_dict.values())[0].values())[0])
+    type_y_fair = get_subtype(list(y_dict.values())[0])
 
     if type_y_true != type_y_fair and threshold is None:
         raise ValueError(