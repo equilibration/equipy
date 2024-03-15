a8bb9e2215fcbf77faaebb3b29d6e0c5791840c9
fer-agathe <agathe.fernandesmachado@gmail.com>
2024-03-15 10:40:52 -0400

    change fair_density_plot with pandas df for sensitive attributes

--- equipy/graphs/_density_plot.py
+++ equipy/graphs/_density_plot.py
@@ -8,18 +8,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from typing import Optional
+from ..fairness._wasserstein import MultiWasserstein
 
 
-def fair_density_plot(y: dict[str, np.ndarray], sensitive_features: np.ndarray) -> plt.Axes:
+def fair_density_plot(sensitive_features_calib: np.ndarray,
+                      sensitive_features_test: np.ndarray,
+                      y_calib: np.ndarray,
+                      y_test: np.ndarray,
+                      epsilon: Optional[float] = None) -> plt.Axes:
     """
     Visualizes the distribution of predictions based on different sensitive features using kernel density estimates (KDE).
 
     Parameters
     ----------
-    y : dict
-        A dictionary containing sequentially fair output datasets.
-    sensitive_features : np.ndarray, shape (n_samples, n_sensitive_features)
-        The samples representing multiple sensitive attributes.
+    sensitive_features_calib : numpy.ndarray
+        Sensitive features for calibration.
+    sensitive_features_test : numpy.ndarray
+        Sensitive features for testing.
+    y_calib : numpy.ndarray
+        Predictions for calibration.
+    y_test : numpy.ndarray
+        Predictions for testing.
+    epsilon : float, optional, default = None
+        Epsilon value for calculating Wasserstein distance
 
     Returns
     -------
@@ -42,29 +54,37 @@ def fair_density_plot(y: dict[str, np.ndarray], sensitive_features: np.ndarray) 
     >>> sensitive_features = [[sensitive_features_of_ind_1_values], [sensitive_feature_of_ind_2_values], ...]
     """
 
-    fig, axes = plt.subplots(nrows=len(sensitive_features.T), ncols=len(
-        sensitive_features.T)+1, figsize=(26, 18))
+    exact_wst = MultiWasserstein()
+    exact_wst.fit(y_calib, sensitive_features_calib)
+    y_final_fair = exact_wst.transform(y_test, sensitive_features_test, epsilon=epsilon)
+    y_sequential_fair = exact_wst.get_sequential_fairness()
+
+    fig, axes = plt.subplots(nrows=sensitive_features_test.shape[1], ncols=
+                         sensitive_features_test.shape[1]+1, figsize=(26, 18))
     fig.suptitle('Density function sequentally fair', fontsize=40)
-
     axes = axes.flatten(order='F')
-
     graph = 0
     modalities = {}
+    x_axes = {}
 
-    for key in y.keys():
+    sensitive_features_test.reset_index(drop=True, inplace=True)
+    for key in y_sequential_fair.keys():
+        if key == 'Base model':
+            x_axes[key] = 'Base model predictions'
+        else:
+            x_axes[key] = f'Fair predictions in {key}'
         df = pd.DataFrame()
-        df['Prediction'] = y[key]
-        title = key
-        for i, sensitive_feature in enumerate(sensitive_features.T):
-            df[f"sensitive_feature_{i+1}"] = sensitive_feature
-            modalities[i] = df[f"sensitive_feature_{i+1}"].unique()
-            for mod in modalities[i]:
-                subset_data = df[df[f'sensitive_feature_{i+1}'] == mod]
+        df['Prediction'] = y_sequential_fair[key]
+        for col in sensitive_features_test.columns:
+            sensitive_feature = sensitive_features_test[col]
+            df[col] = sensitive_feature
+            modalities[col] = df[col].unique()
+            for mod in modalities[col]:
+                subset_data = df[df[col] == mod]
                 sns.kdeplot(
-                    subset_data['Prediction'], label=f'sensitive_feature_{i+1}: {mod}', fill=True, alpha=0.2, ax=axes[graph])
+                    subset_data['Prediction'], label=f'{col}: {mod}', fill=True, alpha=0.2, ax=axes[graph])
             axes[graph].legend(fontsize=15)
-            axes[graph].set_title(title, fontsize=30)
-            axes[graph].set_xlabel('Prediction', fontsize=20)
+            axes[graph].set_xlabel(x_axes[key], fontsize=20)
             axes[graph].set_ylabel('Density', fontsize=20)
             axes[graph].xaxis.set_tick_params(labelsize=20)
             axes[graph].yaxis.set_tick_params(labelsize=20)
